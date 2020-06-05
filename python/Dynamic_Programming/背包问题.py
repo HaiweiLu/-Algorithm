5062742b296c99bc04c26ec14522b649d3be03b1
Haiwei Lu <newknowledgemail@gmail.com>
2020-06-05 20:16:29 +0800

    修改了 背包问题.py

--- python/Dynamic_Programming/背包问题.py
+++ python/Dynamic_Programming/背包问题.py
@@ -4,7 +4,12 @@
 https://segmentfault.com/a/1190000022360607
 http://www.omica.com.cn/news/show-8723.html
 '''
+
 def coinChange(coins, amount):
+    '''零钱兑换
+    coins[List]: 可以兑换的额度
+    amount: 要兑换的额度
+    '''
     dp = [0] + [float('inf')] * amount
     for coin in coins:
         for i in range(coin, amount + 1):
@@ -44,34 +49,7 @@ def traceback(notebook, projectNumber, money):
 
     traceback(notebook, projectNumber-1, money-invest)
 
-def getInvest(project, money):
-    
-    if project is None or money <=0:
-        return
-    
-    m, n = len(project), len(project[0])
-    dp = [[0] * n for _ in range(m)]
-    notebook = [[0] * n for _ in range(m)]
 
-    for i in range(1, n):
-        dp[0][i] = project[0][i]
-        notebook[0][1] = 1
-
-    for i in range(1, m):
-        for j in range(money + 1):
-            Max = 0
-            for k in range(j + 1):
-                if project[i][k] + dp[i-1][j-1] > Max:
-                    Max = project[i][k] + dp[i-1][j-1]
-                    notebook[i][j] = k
-            dp[i][j] = Max
-
-    print("=========")
-    for row in dp:
-        print(row)
-    print("----------")
-    for row in notebook:
-        print(row)
 
 if __name__ == "__main__":
     project = [[0, 15, 28, 40, 51], # project[i][j] 表示项目 i,投资 j 元的收益
@@ -84,7 +62,6 @@ if __name__ == "__main__":
                 [0, 20, 21, 22, 23, 24]]
     print("MaxProfit: {}".format(investDistribute(project, money)))
     # print("MaxProfit: {}".format(investDistribute(project1, money + 1)))
-    # getInvest(project, money)
 
     # coins = [1, 2, 5]
     # amount = 11
